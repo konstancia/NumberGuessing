733aca8a0258d94613e11eae621d32473eb76b65
Blueyeskon <konstancia_milova@hotmail.com>
2025-01-28 21:35:08 -0500

    while loop

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 
 namespace NumberGuessing;
@@ -11,62 +12,67 @@ public class Program
     static void Main(string[] args)
     {
 
-        Console.WriteLine("This exercise is about number guessing");
-        Console.WriteLine(("Please enter a number between 0-10:"));
-
-        //convert number to int32
-        int number = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine($"You have entered: {number}");
-
-        Random rng = new Random();
-        int randomNumber = rng.Next(0, 10);
         bool playAgain = true;
+        bool isCorrectGuess = false;
         string response;
         int numberOfGuesses = 5;
+        Random rng = new Random();
+        int randomNumber = rng.Next(0, 10);
 
-        for (int i = 1; i < 5; i++) 
+        //for (int i = 1; i < 5; i++)
+         
+        while (!isCorrectGuess)
         {
-
-            if (number == randomNumber)
-            {
-                Console.WriteLine("You guessed the correct number");
-            }
             
-            else if (number < randomNumber)
+            Console.WriteLine("This exercise is about number guessing");
+            Console.WriteLine(("Please enter a number between 0-10:"));
+
+            //convert number to int32
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"You have entered: {number}");
+
+            if (number < randomNumber)
             {
                 Console.WriteLine("Your number is too low");
             }
+
             else if (number > randomNumber)
             {
                 Console.WriteLine("Your number is too high");
+            }
+            else
+            {
+                Console.WriteLine("correct");
 
             }
 
             Console.WriteLine($"The correct number is {randomNumber}");
-            
+            isCorrectGuess = true;
+        }
+    }
+}
+
+/*Console.WriteLine("Would you like to play again (Y/N)");
+    response = Console.ReadLine();
+    response = response.ToUpper();
+
+    if (response == "Y")
+    {
+        playAgain = true;
+    }
+    else
+    {
+        playAgain = false;
     }
 
-        Console.WriteLine("Would you like to play again (Y/N)");
-        response = Console.ReadLine();
-        response = response.ToUpper();
-
-        if (response == "Y")
-        {
-            playAgain = true;
-        }
-        else
-        {
-            playAgain = false;
-        }
-
-        Console.WriteLine("Thank you for playing");
-    }
+    Console.WriteLine("Thank you for playing");
+}
 }
 
 /* if (number_Of_Tries == allowed_Tries) ;
 {
-    Console.WriteLine($"The number was: {randomNumber}");
-    playAgain = false;
+Console.WriteLine($"The number was: {randomNumber}");
+playAgain = false;
 }
 */
